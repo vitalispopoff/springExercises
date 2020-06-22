87fe854007380d14316cf7c43c846a363d6abe8f
vitalispopoff <vitalis.popoff@gmail.com>
2020-06-22 23:31:21 +0200

    27.Practice Activity #1 - Inversion of Control with XML Configuration

---
+++ src/spring/exercise/springdemo/SoccerCoach.java
@@ -0,0 +1,11 @@
+package spring.exercise.springdemo;
+
+public class SoccerCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+
+		return "Penalty kick drill: Perform 100 direct hits of a designated foot square area target from a distance of 70 yards";
+	}
+
+}
---
+++ src/spring/exercise/springdemo/SumoCoach.java
@@ -0,0 +1,11 @@
+package spring.exercise.springdemo;
+
+public class SumoCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+
+		return "Gain 50 pounds of a body mass";
+	}
+
+}
