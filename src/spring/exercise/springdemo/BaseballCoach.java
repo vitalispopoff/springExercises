b6f6fa17c405eee4c70e4f98f7ff8b4559f86e83
vitalispopoff <vitalis.popoff@gmail.com>
2020-06-24 00:42:20 +0200

    31. Spring Dependency Injection - Write Some Code - Part 2

--- src/spring/exercise/springdemo/BaseballCoach.java
+++ src/spring/exercise/springdemo/BaseballCoach.java
@@ -1,13 +1,21 @@
 package spring.exercise.springdemo;
 
 public class BaseballCoach implements Coach {
-	
-//	constructor 
-	
-	
+
+	private FortuneService fortuneService;
+
+	public BaseballCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
 		return "Spend 30 minutes on batting practise";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		
+		return fortuneService.getFortune();
 	}
 }
--- src/spring/exercise/springdemo/HappyFortuneService.java
+++ src/spring/exercise/springdemo/HappyFortuneService.java
@@ -4,6 +4,8 @@ public class HappyFortuneService implements FortuneService{
 
 	@Override
 	public String getFortune() {
+		
 		return "Today is your lucky day!";
+		
 	}
 }
