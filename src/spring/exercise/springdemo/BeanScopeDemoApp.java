71f2d81926cc8098886757db5c495959bd9bdf2b
vitalispopoff <vitalis.popoff@gmail.com>
2020-06-24 16:34:36 +0200

    45. Bean Scopes - Write Some Code - Part 2

--- src/spring/exercise/springdemo/BeanScopeDemoApp.java
+++ src/spring/exercise/springdemo/BeanScopeDemoApp.java
@@ -7,6 +7,7 @@ public class BeanScopeDemoApp {
 	public static void main(String[] args) {
 
 //		load the spring configuration file
+		
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
 		
 //		retrieve bean from spring container
@@ -15,7 +16,20 @@ public class BeanScopeDemoApp {
 		
 		Coach alphaCoach = context.getBean("myCoach", Coach.class);
 		
+//		compare the beans
 		
+		boolean result = (theCoach == alphaCoach);
+		
+//		print the result
+		
+		System.out.println("\nPointing to the same object ? "+result);
+		
+		System.out.println("\nMemory location for theCoach :"+ theCoach);
+		System.out.println("\nMemory location for alphaCoach :"+ alphaCoach);
+		
+//		close the context
+		
+		context.close();
 
 	}
 }
