38b3fbf444e30c72df4fbd261400dc198720e54c
vitalispopoff <vitalis.popoff@gmail.com>
2020-06-25 01:23:14 +0200

    71. Qualifiers for Dependency Injection - Write Some Code - Part 2

--- src/spring/exercise/springdemo/TennisCoach.java
+++ src/spring/exercise/springdemo/TennisCoach.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 public class TennisCoach implements Coach {
 	
 	@Autowired
-	@Qualifier("happyFortuneService")
+	@Qualifier("randomFortuneService")
 	private FortuneService fortuneService;
 	
 	public TennisCoach() {
