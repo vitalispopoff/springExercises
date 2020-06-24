8702988bd4707d88bd31fd2330d9f928780bb031
vitalispopoff <vitalis.popoff@gmail.com>
2020-06-24 17:31:52 +0200

    48. Bean Lifecycle - Write Some Code

--- src/spring/exercise/springdemo/TrackCoach.java
+++ src/spring/exercise/springdemo/TrackCoach.java
@@ -13,14 +13,23 @@ public class TrackCoach implements Coach {
 
 	@Override
 	public String getDailyWorkout() {
-
 		return "Run a hard 5k";
 	}
 
 	@Override
 	public String getDailyFortune() {
-
 		return "Just do it! " + fortuneService.getFortune();
+	}
 
+//	add an init method
+	
+	public void doMyStartupStuff() {
+		System.out.println("... TrackCoach : doMyStartupStuff");
+	}
+	
+//	add a destroy method
+	
+	public void doMyCleanupStuffYoYo() {
+		System.out.println("... TrackCoach : doMyCleanupStuffYoYo");
 	}
 }
