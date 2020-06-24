5d6b35003276894b7bf08904a7f94e726f4a52d2
vitalispopoff <vitalis.popoff@gmail.com>
2020-06-24 02:10:29 +0200

    36. Setter Injection - Write Some Code - Part 2

--- src/spring/exercise/springdemo/HappyFortuneService.java
+++ src/spring/exercise/springdemo/HappyFortuneService.java
@@ -1,6 +1,11 @@
 package spring.exercise.springdemo;
 
 public class HappyFortuneService implements FortuneService{
+	
+	public HappyFortuneService() {
+		super();
+		System.out.println("CricketCoach > HappyFrotuneService : no-arg constructor");
+	}
 
 	@Override
 	public String getFortune() {
---
+++ src/spring/exercise/springdemo/SetterDemoApp.java
@@ -0,0 +1,31 @@
+package spring.exercise.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SetterDemoApp {
+
+	public static void main(String[] args) {
+
+//		load the spring configuration file
+
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+
+//		retrieve bean from spring container - why having the bean id w/ established class the context needs CricketCoach.class ?
+		
+		CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
+
+//		call methods
+		
+//		to be done later.
+		
+		System.out.println(theCoach.getDailyWorkout());
+		
+		System.out.println(theCoach.getDailyFortune());
+
+//		close the context
+		
+		context.close();
+
+	}
+}
