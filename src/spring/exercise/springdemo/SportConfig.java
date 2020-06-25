967b4c2486257f13352c55ca5af0c778af0d1ed4
vitalispopoff <vitalis.popoff@gmail.com>
2020-06-25 13:57:14 +0200

    89. Defining Spring Beans with Java Code (no xml) - Part 2

--- src/spring/exercise/springdemo/SportConfig.java
+++ src/spring/exercise/springdemo/SportConfig.java
@@ -3,9 +3,20 @@ package spring.exercise.springdemo;
 import org.springframework.context.annotation.*;
 
 @Configuration
-@ComponentScan("spring.exercise.springdemo")
+//@ComponentScan("spring.exercise.springdemo")	// commenting it out as no autoscans are to be run 
 public class SportConfig {
-	
-	
 
+	//	define bean for the sadFortuneService
+	
+	@Bean	// beanId is sadFortuneService
+	public FortuneService sadFortuneService() {
+		return new SadFortuneService();
+	}
+	
+	//	define bean for the SwimCoach and inject dependency
+	
+	@Bean
+	public Coach swimCoach() {
+		return new SwimCoach(sadFortuneService());
+	}
 }
