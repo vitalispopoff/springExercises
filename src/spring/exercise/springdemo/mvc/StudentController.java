7bedad39a358fcbe3505990bbbe3c967a4df2c43
vitalispopoff <vitalis.popoff@gmail.com>
2020-06-26 14:20:41 +0200

    128. Text Fields - Part 2

--- src/spring/exercise/springdemo/mvc/StudentController.java
+++ src/spring/exercise/springdemo/mvc/StudentController.java
@@ -2,21 +2,29 @@ package spring.exercise.springdemo.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
-	
+
 	@RequestMapping("/showForm")
 	public String showForm(Model theModel) {
-		
-		//	create new student object
+
+		// create new student object
 		Student theStudent = new Student();
-		
-		//	add student object to the model		
+
+		// add student object to the model
 		theModel.addAttribute("student", theStudent);
-		
-		return "student-form";		
-	}	
+
+		return "student-form";
+	}
+
+	@RequestMapping("/processForm")
+	public String processForm(@ModelAttribute("student") Student theStudent) {
+
+		System.out.println("theStudent : " + theStudent.getFirstName() + " " + theStudent.getLastName());
+
+		return "student-confirmation";
+	}
 }
