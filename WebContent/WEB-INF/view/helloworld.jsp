5560a5dccb419a37ab0c661a084900eccd080e1e
vitalispopoff <vitalis.popoff@gmail.com>
2020-06-26 11:57:27 +0200

    116. Adding Data to the Spring Model - Part 2

--- WebContent/WEB-INF/view/helloworld.jsp
+++ WebContent/WEB-INF/view/helloworld.jsp
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
 <html>
 	<body>
-		<h2>
-			Hello World of Spring!  <br>
-			Student name : ${param.studentName}
-		</h2>
+		<h2>Hello World of Spring!</h2>
+		<p><br><hr><br></p>
+	
+		<h3>Student name : ${param.studentName}</h3>
+		
+		<p><br><hr><br></p>
+		
+		<h4>The message : ${message}</h4>
 	</body>
 </html>
--- src/spring/exercise/springdemo/mvc/HelloWorldController.java
+++ src/spring/exercise/springdemo/mvc/HelloWorldController.java
@@ -1,6 +1,9 @@
 package spring.exercise.springdemo.mvc;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,4 +22,27 @@ public class HelloWorldController {
 	public String processForm() {		
 		return "helloworld";
 	}
+	
+	//	controller method to read form data and adata to the model 
+	
+	@RequestMapping("/processFormVersionTwo")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+		
+		//	read the request parameter from the HTML form	
+		String 
+			theName = request.getParameter("studentName");
+		
+		// convert the data to all caps
+		theName = theName.toUpperCase();
+		
+		//	create the message
+		String 
+			result = "Yo! " + theName;
+		
+		//	add the message to the model		
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
+	
 }
