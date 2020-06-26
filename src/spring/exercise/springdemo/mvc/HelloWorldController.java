c927310c9b103d105c21d18027d6d0e587eac8ae
vitalispopoff <vitalis.popoff@gmail.com>
2020-06-26 11:32:17 +0200

    115. Adding Data to the Spring Model - Part 1

--- src/spring/exercise/springdemo/mvc/HelloWorldController.java
+++ src/spring/exercise/springdemo/mvc/HelloWorldController.java
@@ -1,5 +1,22 @@
 package spring.exercise.springdemo.mvc;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HelloWorldController {
 
+	// controller method to show the initial html form
+
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+
+	//	controller method to process the html form
+	
+	@RequestMapping("/processForm")
+	public String processForm() {		
+		return "helloworld";
+	}
 }
