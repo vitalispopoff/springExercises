23bbcbae11a49933ea4350028cad8ae5eb69f076
vitalispopoff <vitalis.popoff@gmail.com>
2020-06-26 15:07:17 +0200

    132. Drop-Down Lists - Part 2

--- src/spring/exercise/springdemo/mvc/Student.java
+++ src/spring/exercise/springdemo/mvc/Student.java
@@ -1,5 +1,6 @@
 package spring.exercise.springdemo.mvc;
 
+import java.util.LinkedHashMap;
 
 public class Student {
 	
@@ -12,9 +13,22 @@ public class Student {
 	private String
 		country;
 	
+	private LinkedHashMap<String, String> 
+		countryOptions;
+	
 
 	
-	public Student() {}
+	public Student() {
+		
+		//	populate country options: used ISO country code
+		countryOptions = new LinkedHashMap<>();
+		
+		countryOptions.put("BR", "Brazil");
+		countryOptions.put("FR", "France");
+		countryOptions.put("DE", "Germany");
+		countryOptions.put("IN", "India");		
+		countryOptions.put("US", "United States of America");
+	}
 	
 	
 
@@ -41,5 +55,9 @@ public class Student {
 	public void setCountry(String country) {
 		this.country = country;
 	}
-		
+
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
+	}
+			
 }
