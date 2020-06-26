bedb80c99901308fbd2d3ca5640cf9b34970ddc2
vitalispopoff <vitalis.popoff@gmail.com>
2020-06-26 15:43:52 +0200

    135. Radio Buttons

--- WebContent/WEB-INF/view/student-confirmation.jsp
+++ WebContent/WEB-INF/view/student-confirmation.jsp
@@ -6,12 +6,17 @@
 
 		<body>
 		
-		
+		<br><br>
+				
 			The student <i>${student.firstName} ${student.lastName}</i> is confirmed.
 		
 		<br><br>
 		
 			Country: ${student.country}	
+			
+		<br><br>
+		
+			Favorite Language: ${student.favoriteLanguage}
 			
 		</body>
 
--- src/spring/exercise/springdemo/mvc/Student.java
+++ src/spring/exercise/springdemo/mvc/Student.java
@@ -16,6 +16,9 @@ public class Student {
 	private LinkedHashMap<String, String> 
 		countryOptions;
 	
+	private String
+		favoriteLanguage;
+	
 
 	
 	public Student() {
@@ -59,5 +62,13 @@ public class Student {
 	public LinkedHashMap<String, String> getCountryOptions() {
 		return countryOptions;
 	}
-			
+
+	public String getFavoriteLanguage() {
+		return favoriteLanguage;
+	}
+
+	public void setFavoriteLanguage(String favoriteLanguage) {
+		this.favoriteLanguage = favoriteLanguage;
+	}
+		
 }
