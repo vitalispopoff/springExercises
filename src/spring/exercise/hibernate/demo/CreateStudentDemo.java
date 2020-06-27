56b1ccb3d64fd6fbd39eb9d3c87503a08c28828f
vitalispopoff <vitalis.popoff@gmail.com>
2020-06-27 13:52:11 +0200

    187. Creating and Saving Java Objects - Part 2

---
+++ src/spring/exercise/hibernate/demo/CreateStudentDemo.java
@@ -0,0 +1,53 @@
+package spring.exercise.hibernate.demo;
+
+import org.hibernate.*;
+import org.hibernate.cfg.Configuration;
+
+import spring.exercise.demo.entity.Student;
+
+public class CreateStudentDemo {
+
+	public static void main(String[] args) {
+
+		//	create session factory
+		SessionFactory 
+			factory = new Configuration()
+						.configure("hibernate.cfg.xml")
+						.addAnnotatedClass(Student.class)
+						.buildSessionFactory();		
+
+		//	create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			//	create a student object
+			
+			System.out.println("...creating a student object");
+			Student tempStudent = new Student("Paul", "Wall", "paul@domain.com");
+			
+			//	start a transaction
+			session.beginTransaction();
+			
+			
+			//	save the student object
+			System.out.println("...saving the stutent");
+			
+			session.save(tempStudent);
+			
+			
+			//	commit transaction
+			session.getTransaction().commit();
+				
+			System.out.println("...done");
+			
+		}
+		finally {
+			factory.close();
+			
+			System.out.println("...session closed.");
+			
+		}
+		
+
+	}
+}
