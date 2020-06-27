5e0978a3e0856d6ab68ce4704a2055f7a2982854
vitalispopoff <vitalis.popoff@gmail.com>
2020-06-27 19:14:44 +0200

    199. Deleting Objects with Hibernate - Overview

---
+++ src/spring/exercise/hibernate/demo/DeleteStudentDemo.java
@@ -0,0 +1,35 @@
+package spring.exercise.hibernate.demo;
+
+import org.hibernate.*;
+import org.hibernate.cfg.Configuration;
+
+import spring.exercise.demo.entity.Student;
+
+public class DeleteStudentDemo {
+
+	public static void main(String[] args) {
+
+		SessionFactory
+			factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+		Session
+			session;
+		String
+			email = "foo@gmail.com";
+		
+		try {
+			
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+			
+			session.createQuery("delete from Student where email='"+email+"'").executeUpdate();		
+			
+			session.getTransaction().commit();
+		}
+		finally {
+			factory.close();
+		}		
+	}
+}
